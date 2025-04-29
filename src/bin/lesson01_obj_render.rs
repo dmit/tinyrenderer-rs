dab4bdf3d920c44c8af0423ee6eda087ecd8fb5b
Dmitri Melnikov <github@dmit.me>
2025-04-29 14:37:06 +0300

    Parse command-line arguments using argh

--- src/bin/lesson01_obj_render.rs
+++ src/bin/lesson01_obj_render.rs
@@ -1,24 +1,33 @@
-use std::{env, mem, path::Path};
+use std::{error::Error, mem, path::PathBuf};
 
+use argh::FromArgs;
 use image::{ImageBuffer, Rgba, imageops::flip_vertical_in_place};
 use tiny::wavefront::{Obj, Tri};
 
 const WHITE: [u8; 4] = [0xff, 0xff, 0xff, 0xff];
 const BLACK: [u8; 4] = [0x00, 0x00, 0x00, 0xff];
 
-fn main() {
-    let model = Obj::from_file(Path::new(&env::args().nth(1).expect("Specify path to model")))
-        .expect("Failed to load model");
-    println!("Loaded model, vertices: {}, faces: {}", model.vertices.len(), model.faces.len());
+/// Render .obj file to image.
+#[derive(Debug, FromArgs)]
+struct Opts {
+    /// path to the model .obj file
+    #[argh(positional)]
+    model_path: PathBuf,
+    /// image width
+    #[argh(option, short = 'w', default = "1000")]
+    width: u32,
+    /// image height
+    #[argh(option, short = 'h', default = "1000")]
+    height: u32,
+}
 
-    let width = env::args()
-        .nth(2)
-        .map_or(1000, |s| s.parse::<u32>().unwrap_or_else(|_| panic!("Invalid width: {s}")));
-    let height = env::args()
-        .nth(3)
-        .map_or(width, |s| s.parse::<u32>().unwrap_or_else(|_| panic!("Invalid height: {s}")));
+fn main() -> Result<(), Box<dyn Error>> {
+    let opts: Opts = argh::from_env();
 
-    let mut img = ImageBuffer::from_pixel(width, height, Rgba(BLACK));
+    let model = Obj::from_file(&opts.model_path)?;
+    println!("Loaded model: {} vertices, {} faces", model.vertices.len(), model.faces.len());
+
+    let mut img = ImageBuffer::from_pixel(opts.width, opts.height, Rgba(BLACK));
 
     for f in model.faces {
         let tri = Tri(
@@ -28,16 +37,18 @@ fn main() {
         );
 
         for l in tri.lines() {
-            let x0 = (l.0.x + 1.0) * width as f32 / 2.0;
-            let y0 = (l.0.y + 1.0) * height as f32 / 2.0;
-            let x1 = (l.1.x + 1.0) * width as f32 / 2.0;
-            let y1 = (l.1.y + 1.0) * height as f32 / 2.0;
+            let x0 = (l.0.x + 1.0) * opts.width as f32 / 2.0;
+            let y0 = (l.0.y + 1.0) * opts.height as f32 / 2.0;
+            let x1 = (l.1.x + 1.0) * opts.width as f32 / 2.0;
+            let y1 = (l.1.y + 1.0) * opts.height as f32 / 2.0;
             draw_line(x0 as u32, y0 as u32, x1 as u32, y1 as u32, &mut img, Rgba(WHITE));
         }
     }
 
     flip_vertical_in_place(&mut img);
-    img.save("out/lesson01.png").expect("Failed to write image data");
+    img.save("out/lesson01.png")?;
+
+    Ok(())
 }
 
 fn draw_line<T>(
